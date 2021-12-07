c13d9239d08864e23cb6580e789a1e180ab28335
Lukas Warode <47723699+lwarode@users.noreply.github.com>
2021-12-07 16:30:52 +0100

    Delete data_preprocessing.py

--- data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-# packages
-import tweepy
-import pandas as pd
-
-# import tokens from config.py file
-from config import consumer_key, consumer_secret, access_token, access_token_secret
-
-print(tweepy.__version__)
